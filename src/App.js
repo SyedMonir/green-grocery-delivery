16448893a8145587b70dc67932522ebc06ae9d5b
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-16 16:51:51 +0600

    added navbar and dark mood

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home/Home';
+
 function App() {
+  const [dark, setDark] = useState(false);
   return (
-    <>
-      <h1>Ready</h1>
-    </>
+    <section data-theme={dark ? 'dark' : 'light'}>
+      <Navbar dark={dark} setDark={setDark} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: '#4E944F',
+          secondary: '#E9EFC0',
+          accent: '#83BD75',
+          neutral: '#F3F4F6',
+          'base-100': 'white',
+          info: '#98A8DD',
+          success: '#1BBB70',
+          warning: '#DF7E07',
+          error: '#FA5C5C',
+        },
+      },
+      {
+        dark: {
+          primary: '#16222A',
+          secondary: '#FDE68A',
+          accent: '#3A6073',
+          neutral: '#F3F4F6',
+          'base-100': '#16222A',
+          info: '#98A8DD',
+          success: '#1BBB70',
+          warning: '#F59E0B',
+          error: '#FB7185',
+        },
+      },
+    ],
   },
   plugins: [require('daisyui')],
 };
